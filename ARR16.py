012735cedcf52e802f11fc7c2458184da0ad4b2b
SriHarshinigithub <129975830+SriHarshinigithub@users.noreply.github.com>
2025-10-27 22:32:56 +0530

    Create ARR16.py

---
+++ ARR16.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        
+        result = []
+        i, j = 0, 0
+
+        # Traverse both strings simultaneously
+        while i < len(word1) or j < len(word2):
+            if i < len(word1):
+                result.append(word1[i])
+                i += 1
+            if j < len(word2):
+                result.append(word2[j])
+                j += 1
+
+        return ''.join(result)
